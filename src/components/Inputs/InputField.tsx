b22447358d44e9b146eb2eb5190c9f95ffa6cbba
Ekanem David <38921132+dueka@users.noreply.github.com>
2023-01-12 15:53:10 +0100

    started creating input field

---
+++ src/components/Inputs/InputField.tsx
@@ -0,0 +1,2 @@
+import React, { useState, useEffect } from "react";
+import { ErrorMessage, useField } from "formik";
--- src/pages/all_employees/CreateEmployee.tsx
+++ src/pages/all_employees/CreateEmployee.tsx
@@ -1,62 +1,74 @@
-import { useEffect, useState } from 'react'
-import Header from '../../components/Header'
-import Sidebar from '../../components/Sidebar'
-import AddEmployeeNav from './AddEmployeeNav';
-import AddEmployeeTitle from './AddEmployeeTitle';
-import Address from './employeeInputs/Address';
-import Employment from './employeeInputs/Employment';
-import Essentials from './employeeInputs/Essentials';
-import Finance from './employeeInputs/Finance';
-import Refrence from './employeeInputs/Refrence';
-import CreateEmployeeView from './employeeInputs/CreateEmployeeView';
+import { useEffect, useState } from "react";
+import { Button } from "@mui/material";
+import { GoArrowLeft } from "react-icons/go";
+import { useNavigate } from "react-router-dom";
+import Header from "../../components/Header";
+import Sidebar from "../../components/Sidebar";
+import AddEmployeeNav from "./AddEmployeeNav";
+import AddEmployeeTitle from "./AddEmployeeTitle";
+import Address from "./employeeInputs/Address";
+import Employment from "./employeeInputs/Employment";
+import Essentials from "./employeeInputs/Essentials";
+import Finance from "./employeeInputs/Finance";
+import Refrence from "./employeeInputs/Refrence";
+import CreateEmployeeView from "./employeeInputs/CreateEmployeeView";
 
 const CreateEmployee = () => {
-	// State to store count value 
-	const [active, setActive] = useState<number>(1)
-	useEffect(() => {
-		setActive(active)
-	}, [active])
+  const navigate = useNavigate();
 
+  // State to store count value
+  const [active, setActive] = useState<number>(1);
+  useEffect(() => {
+    setActive(active);
+  }, [active]);
 
+  const [collapseNav, setCollapseNav] = useState(() => {
+    // @ts-ignore
+    return JSON.parse(localStorage.getItem("collapse")) || false;
+  });
 
-	const [collapseNav, setCollapseNav] = useState(() => {
-		// @ts-ignore
-		return JSON.parse(localStorage.getItem("collapse")) || false;
-	});
+  useEffect(() => {
+    // --- Set state of collapseNav to localStorage on pageLoad --- //
+    localStorage.setItem("collapse", JSON.stringify(collapseNav));
+    // --- Set state of collapseNav to localStorage on pageLoad --- //
+  }, [collapseNav]);
+  const toggleSideNav = () => {
+    setCollapseNav(!collapseNav);
+  };
+  return (
+    <div id="screen-wrapper">
+      <Header toggleSideNav={toggleSideNav} />
+      <Sidebar collapseNav={collapseNav} />
+      <main>
+        <div className="back-to-employee-container">
+          <Button
+            onClick={() => navigate("/allemployees")}
+            variant="outlined"
+            className="back-to-employee-button"
+          >
+            <GoArrowLeft className="back-to-employee-icon" size={20} />
+          </Button>
+        </div>
+        <div className="addemployeecontainer">
+          <AddEmployeeTitle setActive={setActive} active={active} />
+          {active === 6 ? (
+            ""
+          ) : (
+            <AddEmployeeNav active={active} setActive={setActive} />
+          )}
 
-	useEffect(() => {
-		// --- Set state of collapseNav to localStorage on pageLoad --- //
-		localStorage.setItem("collapse", JSON.stringify(collapseNav));
-		// --- Set state of collapseNav to localStorage on pageLoad --- //
-	}, [collapseNav]);
-	const toggleSideNav = () => {
-		setCollapseNav(!collapseNav);
-	};
-	return (
-		<div id="screen-wrapper">
-			<Header toggleSideNav={toggleSideNav} />
-			<Sidebar collapseNav={collapseNav} />
-			<main>
-				<div className='addemployeecontainer'>
-					<AddEmployeeTitle setActive={setActive} active={active} />
-					{active === 6 ? "" : <AddEmployeeNav active={active} setActive={setActive} />}
+          <div className="all-inputs-container">
+            {active === 1 && <Essentials />}
+            {active === 2 && <Finance />}
+            {active === 3 && <Refrence />}
+            {active === 4 && <Employment />}
+            {active === 5 && <Address setActive={setActive} />}
+            {active === 6 && <CreateEmployeeView />}
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+};
 
-					<div className='all-inputs-container'>
-						{active === 1 && <Essentials />}
-						{active === 2 && <Finance />}
-						{active === 3 && <Refrence />}
-						{active === 4 && <Employment />}
-						{active === 5 && <Address setActive={setActive} />}
-						{active === 6 && <CreateEmployeeView />}
-
-					</div>
-				</div>
-			</main>
-		</div>
-	)
-}
-
-export default CreateEmployee
-
-
-
+export default CreateEmployee;
